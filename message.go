154423edefb2f8830708d1ad88a86547a37afe4a
sky0621 <sky14work@gmail.com>
2016-11-13 06:21:09 +0900

    message

--- message.go
+++ message.go
@@ -4,8 +4,8 @@ import "time"
 
 // Message ... １つのメッセージを表す。※イベントが「メッセージ」の場合に使う。
 type Message struct {
-	Name    string
 	Message string
 	When    time.Time
+	Name    string
 	// FIXME あとは、ID、アバターのURLくらいか。
 }
